05b2955ef54c8d0b9d1525a7fa8986928859b518
Richard Tran <richard.tran@rutgers.edu>
2016-08-30 22:49:35 +0000

    Add middleware

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var connect = require('connect');
 var app = connect();
 
+var helloWorld = function(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+};
+
+app.use(helloWorld); // can also simply write function in parameter
 app.listen(process.env.PORT, process.env.IP);
+
 
 console.log('Server running at ' + process.env.IP + ':' + process.env.PORT);
