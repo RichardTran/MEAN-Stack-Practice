305d39553dd0d12f6c36211ff81a972613ffd9c8
Richard Tran <richard.tran@rutgers.edu>
2016-09-01 04:02:08 +0000

    Add stage 2 of Web Dev book

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var connect = require('connect');
 var app = connect();
 
+var logger = function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+};
+
 var helloWorld = function(req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
 };
 
-app.use(helloWorld); // can also simply write function in parameter
+var goodbyeWorld = function(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Goodbye World');
+};
+
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbyeWorld);
+//app.use(helloWorld); // can also simply write function in parameter
 app.listen(process.env.PORT, process.env.IP);
 
 
