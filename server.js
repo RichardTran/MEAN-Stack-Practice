a74680275691d442b0152ac8cc194a9d91c63cd4
Richard Tran <richard.tran@rutgers.edu>
2016-09-01 16:28:59 +0000

    Replace with express

--- server.js
+++ server.js
@@ -1,26 +1,11 @@
-var connect = require('connect');
-var app = connect();
+var express = require('express');
+var app = express();
 
-var logger = function(req, res, next) {
-    console.log(req.method, req.url);
-    next();
-};
+app.use('/', function(req, res) {
+    res.send('Hello World');
+});
 
-var helloWorld = function(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-};
+app.listen(process.env.PORT);
+console.log('Server running at http://localhost:3000/');
 
-var goodbyeWorld = function(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Goodbye World');
-};
-
-app.use(logger);
-app.use('/hello', helloWorld);
-app.use('/goodbye', goodbyeWorld);
-//app.use(helloWorld); // can also simply write function in parameter
-app.listen(process.env.PORT, process.env.IP);
-
-
-console.log('Server running at ' + process.env.IP + ':' + process.env.PORT);
+module.exports = app;
