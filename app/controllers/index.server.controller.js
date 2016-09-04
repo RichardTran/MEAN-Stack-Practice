dcdef7a003b64e73541a5b830b5b249f43b03445
Richard Tran <richard.tran@rutgers.edu>
2016-09-04 00:41:32 +0000

    Add mongoose dependency

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,4 +1,12 @@
 exports.render = function(req, res) {
+    //req.session is an object that actually exists
+    // lastVisit is a variable that we named ourselves.
+    // Code will work the same as req.session.batman
+    if (req.session.lastVisit) {
+        console.log(req.session.lastVisit);
+    }
+    req.session.lastVisit = new Date();
+    
     res.render('index', {
         title: 'Hello World'
     })
