ccc6808607e103742275f87bd79e7d0f22cb2a0e
Richard Tran <richard.tran@rutgers.edu>
2016-09-17 23:41:15 +0000

    Add local user authentication

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,13 +1,6 @@
 exports.render = function(req, res) {
-    //req.session is an object that actually exists
-    // lastVisit is a variable that we named ourselves.
-    // Code will work the same as req.session.batman
-    if (req.session.lastVisit) {
-        console.log(req.session.lastVisit);
-    }
-    req.session.lastVisit = new Date();
-    
     res.render('index', {
-        title: 'Hello World'
-    })
+        title: 'Hello World',
+        userFullName: req.user ? req.user.fullName : ''
+    });
 };
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,15 +1,16 @@
-var users = require('../../app/controllers/users.server.controller');
-
-module.exports = function(app){
-    app.route('/users')
-        .post(users.create)
-        .get(users.list);
+var users = require('../../app/controllers/users.server.controller'),
+    passport = require('passport');
     
-    app.route('/users/:userId')
-        .get(users.read)
-        .put(users.update)
-        .delete(users.delete);
-    
-    // runs before app.route as soon as a request parameter is present (after the colon)
-    app.param('userId', users.userById);
-}
+module.exports = function(app) {
+    app.route('/signup')
+        .get(users.renderSignup)
+        .post(users.signup);
+    app.route('/signin')
+        .get(users.renderSignin)
+        .post(passport.authenticate('local', {
+            successRedirect: '/',
+            failureRedirect: '/signin',
+            failureFlash: true
+        }));
+    app.get('/signout', users.signout);
+};
