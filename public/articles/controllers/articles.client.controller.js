524c9d57d4ffce86797a02925a58069892b3c542
Richard Tran <richard.tran@rutgers.edu>
2016-10-04 09:32:10 +0000

    Add project up to page 212

---
+++ public/articles/controllers/articles.client.controller.js
@@ -0,0 +1,6 @@
+angular.module('articles').controller('ArticlesController', ['$scope',
+	'$routeParams', '$location', 'Authentication', 'Articles',
+	function($scope, $routeParams, $location, Authentication, Articles) {
+		$scope.authentication = Authentication;
+	}
+]);
